45bd4d6b4ee5b788f6b0631bbf327178611db5c0
awmiesch <awmiesch@gmail.com>
2022-08-31 08:26:11 -0400

    updated fhlbof package

--- scripts/fhlbof/__main__.py
+++ scripts/fhlbof/__main__.py
@@ -2,7 +2,7 @@
 Convert each .dat file into a .csv file.
 """
 
-import sys, argparse, re, pandas, yaml
+import sys, argparse, re, datetime, pandas, yaml
 from pathlib import Path
 
 
@@ -33,16 +33,17 @@ def transform(column, metadata):
     return fn(column)
 
 
-def main(datadir: Path, outputdir: Path, sourcedir: Path):
+def main(datadir: Path, outputdir: Path, effective_date: str, sourcedir: Path):
 
-    for properties_file in sourcedir.glob("*.yml"):
-        
-        table_name = properties_file.stem
+    for properties_file in sourcedir.glob("*.yml"):     
+
+        table_name = properties_file.stem   
 
         with open(properties_file, 'r') as stream:
             properties = yaml.load(stream, Loader=yaml.SafeLoader)
         
-        datafile = datadir.joinpath(f"{table_name}.dat")
+        # Ignore the first 3 characters of the filename.
+        datafile = [x for x in datadir.glob(f"*{table_name}.dat")][0]
     
         with open(datafile, 'r') as stream:
             rows = stream.readlines()
@@ -72,6 +73,7 @@ def main(datadir: Path, outputdir: Path, sourcedir: Path):
             transformed_columns[colname] = transform(column, metadata)
         
         table = pandas.DataFrame(transformed_columns)
+        table['EFFECTIVE_DATE'] = datetime.date.fromisoformat(effective_date)
 
         output_file = outputdir.joinpath(f"{table_name}.csv")
         table.to_csv(
@@ -97,6 +99,11 @@ if __name__ == "__main__":
         help="Path to output file directory. Defaults to user home directory.",
     )
     argparser.add_argument(
+        "effective_date",
+        type=str,
+        help="Effective date in YYYY-MM-DD format",
+    )
+    argparser.add_argument(
         "--sourcedir",
         type=Path,
         help="Path to directory containing yaml properties file for each table. Defaults to fhlbof/tabledefs.",
@@ -105,5 +112,5 @@ if __name__ == "__main__":
 
     args = argparser.parse_args()
     
-    main(args.datadir, args.outputdir, args.sourcedir)
+    main(args.datadir, args.outputdir, args.effective_date, args.sourcedir)
 
