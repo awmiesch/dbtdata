1ded7c50d45751ffa326d252998e6bf6f33f0298
awmiesch@gmail.com <awmiesch@gmail.com>
2022-08-31 17:21:43 +0000

    dbt_dev1 updates

--- models/staging/fhlbof/stg_cpn.sql
+++ models/staging/fhlbof/stg_cpn.sql
@@ -6,7 +6,11 @@ select
     coupon,
     margin,
     simpndx as simp_index,
-    firstrst as first_reset_date,
+    case 
+      when firstrst != 'N/A' 
+      then date(cast(substr(firstrst, 1, 4) as int), cast(substr(firstrst, 5, 2) as int), cast(substr(firstrst, 7, 2) as int)) 
+      else null 
+    end as first_reset_date
     cpnfrq as coupon_freq,
     formula
 from {{ source('fhlbof', 'cpn') }}
